be90389208b0dd95c9760ce5d98e1e9b8a6848b7
cwy007 <chanweiyan007@gmail.com>
2017-11-25 22:13:40 +0800

    update current_temp

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,5 +3,16 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
-  
+
+  def update_temp                 # NOTE: method 就是一组处理的集合
+    city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "b73eb66dddf02d2f049c2d84ebea4c05" }
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+  end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -6,8 +6,8 @@ namespace :dev do
 
     data["result"].each do |c|
       existing_city = City.find_by_juhe_id(c["id"])
-      if existing_city.nil?                         # NOTE: nil? method 
-        City.create!(:juhe_id => c["id"], :province => c["province"],
+      if existing_city.nil?                         # NOTE: nil? method
+          City.create!(:juhe_id => c["id"], :province => c["province"],
                      :city => c["city"], :district => c["district"])
       end
     end
