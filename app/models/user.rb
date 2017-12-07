eff1d9fbed6fc492bc4b33f6a63a086791f11535
cwy007 <chanweiyan007@gmail.com>
2017-12-08 02:05:25 +0800

    generate authentication_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
-  has_many :reservations 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :reservations
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token  # NOTE: 
+  end
 end
