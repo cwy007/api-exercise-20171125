aacd062e9db18b77a9298a33b00f1221741bb737
cwy007 <chanweiyan007@gmail.com>
2017-12-07 23:16:26 +0800

    add train and reservation model

---
+++ app/models/reservation.rb
@@ -0,0 +1,10 @@
+class Reservation < ApplicationRecord
+  belongs_to :train
+  before_validation :generate_booking_code, :on => :create 
+  validates_presence_of :train_id, :seat_number, :booking_code
+  validates_uniqueness_of :seat_number, :scope => :train_id
+
+  def generate_booking_code
+    self.booking_code = SecureRandom.uuid
+  end
+end
