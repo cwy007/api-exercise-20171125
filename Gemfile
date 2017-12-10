f7fdd9b17d7026eb42125036a4ae69e9fcc1f6ee
cwy007 <chanweiyan007@gmail.com>
2017-12-11 02:25:55 +0800

    implement GET AND PATCH api/v1/me api

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'rest-client'
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails'
 gem 'devise'
+gem 'carrierwave'
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         
+  mount_uploader :avatar, AvatarUploader
   has_many :reservations
   before_create :generate_authentication_token
 
   def generate_authentication_token
-    self.authentication_token = Devise.friendly_token  # NOTE: 
+    self.authentication_token = Devise.friendly_token  # NOTE:
   end
 end
