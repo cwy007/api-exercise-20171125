1221a3608a8468b5b9393c3fdc9f6a027a918270
cwy007 <chanweiyan007@gmail.com>
2017-12-14 15:17:36 +0800

    test api::v1::users get&patch /me apis

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,10 @@ class Api::V1::UsersController < ApiController
       :email => current_user.email,
       :avatar => current_user.avatar,
       :updated_at => current_user.updated_at,
-      :created_at => current_user.created_at 
+      :created_at => current_user.created_at
     }
   end
+  
   def update
     if current_user.update(user_params)
       render :json => { :message => "OK" }
